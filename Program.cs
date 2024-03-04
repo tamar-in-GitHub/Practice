5691fdf868c259fe0753902b460e811bec026748
Tamar <T0533169540@gmail.com>
2024-03-04 14:21:17 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GitProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
