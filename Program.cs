8ae0e24ea22cd5d2d592085522dee805f3b3f94a
tamar-in-GitHub <151876058+tamar-in-GitHub@users.noreply.github.com>
2024-03-05 15:30:23 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,11 +4,11 @@
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
+
             Console.WriteLine("Hello, tamar 6!");
-=======
+
             Console.WriteLine("Hello Racheli this is your branch!! ");
->>>>>>> 343eb05c1aa76d7e937f76f5103948f3fa3d0a89
+
         }
     }
 }
