6e68b2263bd9b8b80cb9cd0b7a9167841c4f4688
tamar-in-GitHub <151876058+tamar-in-GitHub@users.noreply.github.com>
2024-03-05 15:24:10 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,10 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, tamar GitProject5!");
-
-            Console.WriteLine("Hello Racheli & Ahuvi! ");
-
+<<<<<<< HEAD
+            Console.WriteLine("Hello, tamar 6!");
+=======
+            Console.WriteLine("Hello Racheli this is your branch!! ");
+>>>>>>> 343eb05c1aa76d7e937f76f5103948f3fa3d0a89
         }
     }
 }
