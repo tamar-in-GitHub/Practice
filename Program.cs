f998554ed8d886739b46e81645c7d04420c1e5ae
User <had4059@gmail.com>
2024-03-05 14:07:54 +0200

    Branchcommit

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@
 <<<<<<< HEAD
             Console.WriteLine("Hello, tamar!");
 =======
-            Console.WriteLine("Hello Racheli & Ahuvi! ");
+            Console.WriteLine("Hello Racheli this is your branch!! ");
 >>>>>>> 343eb05c1aa76d7e937f76f5103948f3fa3d0a89
         }
     }
